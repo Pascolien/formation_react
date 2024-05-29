149cec106ceed8060995d6f47198c201630c3053
Pascolien <don.scolien@gmail.com>
2024-05-29 12:33:57 +0200

    return explicite

--- 04_arrow.js
+++ 04_arrow.js
@@ -7,7 +7,11 @@
  *   - le mot clée "function" est interdis
  */
 
-let arrow1 = a => a
+let arrow1 = a => {
+    return a+1
+}
+   
+
 
 
 /**
@@ -19,8 +23,8 @@ let arrow1 = a => a
  *   - le mot clée "return" est interdis
  */
 
-let arrow2 = (a, b ) => a+1 
+let arrow2 = a => a+1 
 console.log(arrow1(3));
-console.log(arrow2(2,3));
+console.log(arrow2(2));
 
 module.exports = {arrow1, arrow2};
